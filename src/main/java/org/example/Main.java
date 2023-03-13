1dbd5a7c2abda6bed67677f59cdd3462c3a6963c
pintor1331 <ohlit12@gmail.com>
2023-03-13 13:22:32 +0900

    test-03-13

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,69 @@
 package org.example;
 
 import java.sql.SQLOutput;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         // 프로그램의 시작점
+        System.out.println("===============================================");
 
-        // 문제 : 아래와 같이 출력해주세요.
-        // 문제 : 단을 * 1000까지 출력해주세요.
+//        MallardDuck mallardDuck = new MallardDuck();
+//        mallardDuck.fly();
+//
+//        WhiteDuck whiteDuck = new WhiteDuck();
+//        whiteDuck.fly();
+//
+//        RubberDuck rubberDuck = new RubberDuck();
+//        rubberDuck.fly();
+//
+//        JuniorRubberDuck juniorRubberDuck = new JuniorRubberDuck();
+//        juniorRubberDuck.fly();
 
-        int dan = 8;
+        String s = "abc";
+        String answer = "";
+        int count = 0;
 
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    count++;
+                }
+            }
+            if (count == 1) {
+                answer += s.charAt(i);
+            }
 
-        int i = 1000; // 초기화
-
-        while (i > 0 /* 조건 */) {
-            System.out.println(dan + " * " + i + " = " + dan * i--); // 출력 & 증가
+            count = 0;
         }
 
+        System.out.println(answer);
 
-        // 프로그램의 끝점XX
+
+        // 프로그램의 끝점
     }
+}
+
+class Duck {
+    void fly() {
+        System.out.println("Duck flies.");
+    }
+}
+
+class MallardDuck extends Duck {
+
+}
+
+class WhiteDuck extends Duck {
+
+}
+
+class RubberDuck extends Duck {
+    void fly() {
+        System.out.println("RubberDuck cannot fly.");
+    }
+}
+
+class JuniorRubberDuck extends RubberDuck {
+
 }
