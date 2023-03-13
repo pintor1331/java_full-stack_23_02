d459d430fd90b73891ec69061f2d5ce1c0bc988d
pintor1331 <ohlit12@gmail.com>
2023-03-13 13:42:11 +0900

    test23erwer

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,19 +7,19 @@ public class Main {
     public static void main(String[] args) {
         // 프로그램의 시작점
         System.out.println("===============================================");
-
-        MallardDuck mallardDuck = new MallardDuck();
-        mallardDuck.fly();
-
-        WhiteDuck whiteDuck = new WhiteDuck();
-        whiteDuck.fly();
-
-        RubberDuck rubberDuck = new RubberDuck();
-        rubberDuck.fly();
-
-        JuniorRubberDuck juniorRubberDuck = new JuniorRubberDuck();
-        juniorRubberDuck.fly();
-
+//
+//        MallardDuck mallardDuck = new MallardDuck();
+//        mallardDuck.fly();
+//
+//        WhiteDuck whiteDuck = new WhiteDuck();
+//        whiteDuck.fly();
+//
+//        RubberDuck rubberDuck = new RubberDuck();
+//        rubberDuck.fly();
+//
+//        JuniorRubberDuck juniorRubberDuck = new JuniorRubberDuck();
+//        juniorRubberDuck.fly();
+        String str = "1231ADRFWER3223";
 
 
         // 프로그램의 끝점
